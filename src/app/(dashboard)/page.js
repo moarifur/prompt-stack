1e9afbc35a97b87cb2a175c1c478147e5b01e151
arif <moarifur@gmail.com>
2025-10-13 19:01:06 +0600

    Story 1.1: Project Initialization completed

--- src/app/(dashboard)/page.js
+++ src/app/(dashboard)/page.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import {Button} from "@/components/ui/button";
 
 const HomePage = () => {
     return (
         <>
-          Hello
+          <h1 className={`text-5xl`}>Hello</h1>
+            <Button>Yo</Button>
         </>
     );
 };
